767a1a00e1a27023c93c2840df854e49c3727743
Riya Bisht <79960426+chococandy63@users.noreply.github.com>
2023-11-30 19:21:24 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,63 +1,69 @@
-from openai import OpenAI
-import time
-import pyttsx3
+import numpy as np
+import nltk
+import string
+import random
+f=open('chatbots.txt','r',errors='ignore')
+raw_doc=f.read()
+raw_doc=raw_doc.lower()
+nltk.download('punkt')
+nltk.download('wordnet')
 
-# Set your OpenAI API key
-openai_api_key = "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"
-client = OpenAI(api_key=openai_api_key)
+sent_tokens=nltk.sent_tokenize(raw_doc)
+word_tokens=nltk.word_tokenize(raw_doc)
+sent_tokens[:2] #2 sentences from start
+nltk.download('omw-1.4')
+lemmer = nltk.stem.WordNetLemmatizer()
+def LemTokens(tokens):
+  return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict=dict((ord(punct),None ) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
-messages = []
-system_msg = "permanent_chatbot"  # Set your desired chatbot type here
-messages.append({"role": "system", "content": system_msg})
+GREET_INPUTS=("hello","hi","greetings","sup","whats up","hey")
+GREET_RESPONSES=["hi","hey","*nods*","hi there","hello","i am glad! you are talking to me "]
+def greet(sentence):
+    for word in sentence.split():
+        if word.lower() in GREET_INPUTS:
+            return random.choice(GREET_RESPONSES)
 
-print("Assistant Friday is ready!")
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+def response(user_response):
+    robo1_response=''
+    TfidfVec=TfidfVectorizer(tokenizer=LemNormalize,stop_words='english')
+    tfidf=TfidfVec.fit_transform(sent_tokens)
+    vals=cosine_similarity(tfidf[-1],tfidf)
+    idx=vals.argsort()[0][-2]
+    flat=vals.flatten()
+    flat.sort()
+    req_tfidf=flat[-2]
+    if(req_tfidf==0):
+      robo1_response=robo1_response+"i am sorry i dont unserstand you"
+      return robo1_response
+    else:  
+      robo1_response=robo1_response+sent_tokens[idx]
+      return robo1_response
 
-response_count = 0
-start_time = time.time()
 
-engine = pyttsx3.init()  # Initialize text-to-speech engine
-
-while True:
-    user_input = input("-----------------------------------------------------------------\nYou: ")
-    if user_input.lower() == "quit()":
-        break
-
-    response_count += 1
-    if response_count > 2:
-        elapsed_time = time.time() - start_time
-        if elapsed_time < 60:
-            print("Please wait for 1 minute before the next response.")
-            time.sleep(60 - elapsed_time)
-            start_time = time.time()
-            response_count = 0
-
-    # Ensure the 'prompt' parameter is present
-    prompt = f"You are a helpful assistant.\nUser: {user_input}\n"
-    
-    response = client.completions.create(
-        model="gpt-3.5-turbo",
-        prompt=prompt,
-        max_tokens=150,
-    )
-    
-    assistant_reply = response["choices"][0]["message"]["content"]
-
-    # Split the assistant's reply into multiple lines if more than 100 characters
-    words = assistant_reply.split()
-    formatted_reply = ""
-    line_length = 0
-    for word in words:
-        if line_length + len(word) > 100:
-            formatted_reply += "\n"
-            line_length = 0
-        formatted_reply += word + " "
-        line_length += len(word) + 1
-
-    messages.append({"role": "assistant", "content": assistant_reply})
-    print("Friday:", formatted_reply + "\n")
-
-    # Ask the user whether to speak the assistant's response
-    speak_response = input("Do you want Alex to speak the response? (Y/N): ")
-    if speak_response.lower() == "y":
-        engine.say(formatted_reply)
-        engine.runAndWait()
+flag=True 
+print("BOT: My name is Riya. Let's have a conversation also, if you want to exit any time, just type Bye...")
+while(flag==True):
+    user_response=input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thankyou'):
+            flag=False
+            print("BOT: You are welcome")
+        else:
+            if(greet(user_response)!=None): 
+                print("bot :"+greet(user_response))  
+            else:
+                sent_tokens.append(user_response)
+                word_tokens=word_tokens+nltk.word_tokenize(user_response)
+                final_words=list(set(word_tokens))
+                print("bot :",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("bot :goodbye take care") 
